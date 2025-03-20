e225338b9484cb3b8889603cc03613b876a86ec0
mehedi <mehedi25122003@gmail.com>
2025-03-20 12:48:13 +0600

    react router setup

--- eslint.config.js
+++ eslint.config.js
@@ -9,11 +9,13 @@ export default [
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
+      node:true,
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
+        
       },
     },
     plugins: {
