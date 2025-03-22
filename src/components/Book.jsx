7ce29e87bdcd5395d799029fcf9be30aaaf980cd
mehedi <mehedi25122003@gmail.com>
2025-03-22 16:30:08 +0600

    border added

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -18,15 +18,15 @@ const Book = ({ book }) => {
           <img src={image} alt={bookName} className="rounded-xl w-full  md:max-h-[300px] bg-gray-300" />
         </figure>
         
-        <div className="card-body  border-b-2 border-dashed">
-        <div className="flex justify-between ">
+        <div className="card-body">
+          <div className="flex justify-between ">
             <p className="font-semibold text-[#23be0a]">Pages:{totalPages}</p>
             <p className="font-semibold  text-[#23be0a]">Publishing Year: {yearOfPublishing}</p>
           </div>
           <h2 className="card-title line-clamp-1 text-2xl">{bookName}</h2>
           <p className="text-sm">By : {author}</p>
         </div>
-        <div className="flex justify-between items-center">
+        <div className="flex border-t-2 mx-6 border-dashed justify-between items-center">
           <p>{category}</p>
           <p className="flex gap-1 items-center">
             {rating} <FaRegStar></FaRegStar>
