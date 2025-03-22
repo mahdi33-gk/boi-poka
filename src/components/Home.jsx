a3950c42cd000112d62ec172be1f9c71b7753b49
mehedi <mehedi25122003@gmail.com>
2025-03-22 14:12:17 +0600

    book and banner section added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,15 @@
 import React from "react";
-import books from '../images/books.jpg'
+
+import Books from "./Books";
+import Banner from "./Banner";
 
 const Home = () => {
   return (
-    <div className="hero bg-base-200 min-h-screen rounded-md">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <img
-          src={books}
-          className="max-w-sm rounded-lg shadow-2xl"
-        />
-        <div>
-          <h1 className="text-5xl font-bold">Books to freshen up your bookshelf</h1>
-          <button className="btn btn-primary mt-16">View The List</button>
-        </div>
-      </div>
+    <div>
+      <Banner></Banner>
+      <Books></Books>
     </div>
+    
   );
 };
 
