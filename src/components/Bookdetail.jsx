11b630d3d19b610deaa262f78536eecf828dc130
mehedi <mehedi25122003@gmail.com>
2025-03-22 23:19:34 +0600

    details section started

--- src/components/Bookdetail.jsx
+++ src/components/Bookdetail.jsx
@@ -7,9 +7,17 @@ const Bookdetail = () => {
     const data = useLoaderData()
     const book =data.find(book=> book.bookId === intId)
     
+    
     return (
         <div>
-            <h1>bookDetails {bookId}</h1>
+            <div>
+            <img className='w-20' src={book.image} alt="" />
+            </div>
+            <h1>Name: {book.bookName} </h1>
+            <div className='flex gap-3'>
+            <button class="btn btn-outline">Read</button>
+            <button class="btn btn-accent text-white">WishList</button>
+            </div>
         </div>
     );
 };
