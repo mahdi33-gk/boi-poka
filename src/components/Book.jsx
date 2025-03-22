8134156500abde6ec0598557334491637dbfa304
mehedi <mehedi25122003@gmail.com>
2025-03-22 15:44:36 +0600

    fouhnd error in card section

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -5,23 +5,23 @@ const Book = ({ book }) => {
   const { bookName, author, image, rating, category } = book;
   return (
     <div className="">
-      <div className="card bg-base-100 w-96 shadow-sm">
+      <div className="card bg-base-100 border-2 px-4 py-2 shadow-sm">
         <figure className="px-10 pt-10">
           <img
             src={image}
             alt="Shoes"
-            className="rounded-xl"
+            className="rounded-xl md:max-h-36"
           />
         </figure>
-        <div className="card-body items-center text-center border-b-2 border-dashed">
-          <h2 className="card-title">{bookName}</h2>
-          <p>
-            by- {author}
+        <div className="card-body  border-b-2 border-dashed">
+          <h2 className="card-title text-2xl">{bookName}</h2>
+          <p className="text-sm">
+            By : {author}
           </p>
         </div>
-        <div>
+        <div className="flex justify-between items-center">
             <p>{category}</p>
-            <p>{rating} <FaRegStar></FaRegStar></p>
+            <p className="flex gap-1 items-center">{rating} <FaRegStar></FaRegStar></p>
         </div>
       </div>
     </div>
