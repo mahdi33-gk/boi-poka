dd8f281f28f8f214bfff5cbbefd484beb2a95f54
mehedi <mehedi25122003@gmail.com>
2025-03-22 14:28:17 +0600

    book card start

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React from "react";
 
-const Book = ({book}) => {
-    const {bookName} = book;
-    return (
-        <div>
-            <h3>{bookName}</h3>
+const Book = ({ book }) => {
+  const { bookName, author, image, rating, category } = book;
+  return (
+    <div className="">
+      <div className="card bg-base-100 w-96 shadow-sm">
+        <figure className="px-10 pt-10">
+          <img
+            src={image}
+            alt="Shoes"
+            className="rounded-xl"
+          />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">{bookName}</h2>
+          <p>
+            by- {author}
+          </p>
+          
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Book;
