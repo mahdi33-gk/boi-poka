3375a091beca3ada5c1a5c818aaf70fadf27aacc
mehedi <mehedi25122003@gmail.com>
2025-03-23 11:36:27 +0600

    details section added

--- src/components/Bookdetail.jsx
+++ src/components/Bookdetail.jsx
@@ -1,25 +1,55 @@
-import React from 'react';
-import { useLoaderData, useParams } from 'react-router-dom';
+import React from "react";
+import { useLoaderData, useParams } from "react-router-dom";
 
 const Bookdetail = () => {
-    const {bookId} = useParams();
-    const intId = parseInt(bookId)
-    const data = useLoaderData()
-    const book =data.find(book=> book.bookId === intId)
-    
-    
-    return (
-        <div>
-            <div>
-            <img className='w-20' src={book.image} alt="" />
-            </div>
-            <h1>Name: {book.bookName} </h1>
-            <div className='flex gap-3'>
-            <button class="btn btn-outline">Read</button>
-            <button class="btn btn-accent text-white">WishList</button>
-            </div>
+  const { bookId } = useParams();
+  const intId = parseInt(bookId);
+  const data = useLoaderData();
+  const book = data.find((book) => book.bookId === intId);
+
+  return (
+    <div className="flex gap-3">
+      <div className="bg-gray-100 px-6 py-2">
+        <img className="max-w-full h-full" src={book.image} alt="" />
+      </div>
+      <div>
+        <h1 className="text-4xl font-bold">Name: {book.bookName} </h1>
+        <p>By: {book.author}</p>
+        <div className="divider"></div>
+        <p>{book.category}</p>
+        <div className="divider"></div>
+        <p>
+          <span className="font-semibold">Review:</span> {book.review}
+        </p>
+        <p className="font-semibold">
+          Tags:<span className="text-[#23be0a] font-normal"> {book.tags}</span>
+        </p>
+        <div className="divider"></div>
+        <div className="grid grid-cols-3">
+          <div className="col-span-1">
+            <p className="flex justify-between">
+              Number of pages:
+              <span className="font-semibold">{book.totalPages}</span>{" "}
+            </p>
+            <p className="flex justify-between">
+              Publisher:<span className="font-semibold">{book.publisher}</span>{" "}
+            </p>
+            <p className="flex justify-between">
+              Year of publishing:
+              <span className="font-semibold">{book.yearOfPublishing}</span>{" "}
+            </p>
+            <p className="flex justify-between">
+              Ratings:<span className="font-semibold">{book.rating}</span>{" "}
+            </p>
+          </div>
         </div>
-    );
+        <div className="flex gap-3 mt-3">
+          <button class="btn btn-outline">Read</button>
+          <button class="btn btn-accent text-white">WishList</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Bookdetail;
