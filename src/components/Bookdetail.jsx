595f4f1bb43656e3015ad5db61bb4b8ca4c21cdb
mehedi <mehedi25122003@gmail.com>
2025-03-23 11:47:28 +0600

    card section done

--- src/components/Bookdetail.jsx
+++ src/components/Bookdetail.jsx
@@ -8,46 +8,51 @@ const Bookdetail = () => {
   const book = data.find((book) => book.bookId === intId);
 
   return (
-    <div className="flex gap-3">
-      <div className="bg-gray-100 px-6 py-2">
-        <img className="max-w-full h-full" src={book.image} alt="" />
-      </div>
-      <div>
-        <h1 className="text-4xl font-bold">Name: {book.bookName} </h1>
-        <p>By: {book.author}</p>
-        <div className="divider"></div>
-        <p>{book.category}</p>
-        <div className="divider"></div>
-        <p>
-          <span className="font-semibold">Review:</span> {book.review}
-        </p>
-        <p className="font-semibold">
-          Tags:<span className="text-[#23be0a] font-normal"> {book.tags}</span>
-        </p>
-        <div className="divider"></div>
-        <div className="grid grid-cols-3">
-          <div className="col-span-1">
-            <p className="flex justify-between">
-              Number of pages:
-              <span className="font-semibold">{book.totalPages}</span>{" "}
-            </p>
-            <p className="flex justify-between">
-              Publisher:<span className="font-semibold">{book.publisher}</span>{" "}
-            </p>
-            <p className="flex justify-between">
-              Year of publishing:
-              <span className="font-semibold">{book.yearOfPublishing}</span>{" "}
-            </p>
-            <p className="flex justify-between">
-              Ratings:<span className="font-semibold">{book.rating}</span>{" "}
-            </p>
+    <div className="grid grid-cols-2 gap-3 mb-3">
+        <div className="bg-gray-100 col-span-1 flex justify-center items-center px-6 py-2">
+          <img className="max-w-full h-full" src={book.image} alt="" />
+        </div>
+        <div>
+          <h1 className="text-4xl font-bold">Name: {book.bookName} </h1>
+          <p>By: {book.author}</p>
+          <div className="divider"></div>
+          <p>{book.category}</p>
+          <div className="divider"></div>
+          <p>
+            <span className="font-semibold">Review:</span> {book.review}
+          </p>
+          <p className="font-semibold">
+            Tags:
+            <span className="text-[#23be0a] font-normal"> {book.tags}</span>
+          </p>
+          <div className="divider"></div>
+          <div className="grid grid-cols-3">
+            <div className="col-span-1">
+              <p className="flex justify-between">
+                Number of pages:
+                <span className="font-semibold">{book.totalPages}</span>{" "}
+              </p>
+              <p className="flex justify-between">
+                Publisher:
+                <span className="font-semibold">{book.publisher}</span>{" "}
+              </p>
+              <p className="flex justify-between">
+                Year of publishing:
+                <span className="font-semibold">
+                  {book.yearOfPublishing}
+                </span>{" "}
+              </p>
+              <p className="flex justify-between">
+                Ratings:<span className="font-semibold">{book.rating}</span>{" "}
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-3 mt-3">
+            <button class="btn btn-outline">Read</button>
+            <button class="btn btn-accent text-white">WishList</button>
           </div>
         </div>
-        <div className="flex gap-3 mt-3">
-          <button class="btn btn-outline">Read</button>
-          <button class="btn btn-accent text-white">WishList</button>
-        </div>
-      </div>
+      
     </div>
   );
 };
